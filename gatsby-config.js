6a6a9bd33aff69497e66c221adcc0a84f1ecca3b
Willian Justen <willianjustenqui@gmail.com>
2019-08-03 21:07:08 -0300

    Cleanup starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My blog`,
+    description: `A blog about frontend development and other cool stuff.`,
+    author: `@myblog`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404</h1>
+    <p>Ops...não tem nada aqui.</p>
   </Layout>
 )
 
