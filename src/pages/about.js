f546c75fae7cf21dcb651a54218d1516f0e0693a
Willian Justen <willianjustenqui@gmail.com>
2019-08-03 21:49:45 -0300

    Using gatsby link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react"
+import { Link } from "gatsby"
 
 const AboutPage = () => (
-  <h1>About Page</h1>
+  <>
+    <h1>About Page</h1>
+    <ul>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <Link to="/" activeStyle={{ color: "red" }}>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/about" activeStyle={{ color: "red" }}>
+          About (Gatsby Link)
+        </Link>
+      </li>
+    </ul>
+  </>
 )
 
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,6 +8,19 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
+    <ul>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <Link to="/" activeStyle={{ color: "red" }}>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/about">About (Gatsby Link)</Link>
+      </li>
+    </ul>
   </Layout>
 )
 
