02a6a04aca88b0fe5a9c535bf8faffea9108564a
Willian Justen <willianjustenqui@gmail.com>
2019-08-07 20:49:14 -0300

    Refactor to `useStaticQuery`

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,31 +1,30 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
-const Profile = () => (
-  <StaticQuery
-    query={graphql`
-      query MySiteMetadata {
-        site {
-          siteMetadata {
-            title
-            position
-            description
-          }
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata: { title, position, description },
+    },
+  } = useStaticQuery(graphql`
+    query MySiteMetadata {
+      site {
+        siteMetadata {
+          title
+          position
+          description
         }
       }
-    `}
-    render={({
-      site: {
-        siteMetadata: { title, position, description },
-      },
-    }) => (
-      <div className="Profile-wrapper">
-        <h1>{title}</h1>
-        <h2>{position}</h2>
-        <p>{description}</p>
-      </div>
-    )}
-  />
-)
+    }
+  `)
+
+  return (
+    <div className="Profile-wrapper">
+      <h1>{title}</h1>
+      <h2>{position}</h2>
+      <p>{description}</p>
+    </div>
+  )
+}
 
 export default Profile
