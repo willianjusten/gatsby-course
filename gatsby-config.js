7a0b6d370ef9b73a172e3ab11fcdaa72827e7553
Willian Justen <willianjustenqui@gmail.com>
2019-08-09 18:55:55 -0300

    Add and configure `styled-components`

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     author: `@myblog`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
