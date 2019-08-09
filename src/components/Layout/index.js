53583b353eb1f4602c59ca26c111e0e7c2022377
Willian Justen <willianjustenqui@gmail.com>
2019-08-09 19:34:09 -0300

    Add styles to Layout Component

---
+++ src/components/Layout/index.js
@@ -0,0 +1,33 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Profile from "../Profile"
+
+import styled from "styled-components"
+
+const LayoutWrapper = styled.section`
+  display: flex;
+`
+
+const LayoutMain = styled.main`
+  background: #16202c;
+  min-height: 100vh;
+  padding: 0 3.75rem 0 20rem;
+  width: 100%;
+`
+
+const Layout = ({ children }) => {
+  return (
+    <LayoutWrapper>
+      <aside>
+        <Profile />
+      </aside>
+      <LayoutMain>{children}</LayoutMain>
+    </LayoutWrapper>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
