48956373fb258db2a6f7df7fa04492fc4d14b70d
Willian Justen <willianjustenqui@gmail.com>
2019-08-09 21:40:24 -0300

    Create SocialLinks Content

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,15 @@
+import { Github } from "styled-icons/boxicons-logos/Github"
+import { Twitter } from "styled-icons/boxicons-logos/Twitter"
+import { Youtube } from "styled-icons/boxicons-logos/Youtube"
+import { Unsplash } from "styled-icons/boxicons-logos/Unsplash"
+import { Instagram } from "styled-icons/boxicons-logos/Instagram"
+
+const Icons = {
+  Github,
+  Twitter,
+  Youtube,
+  Unsplash,
+  Instagram,
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,26 @@
+const handle = "johndoe"
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/${handle}`,
+  },
+  {
+    label: "Youtube",
+    url: `https://www.youtube.com/${handle}`,
+  },
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/${handle}`,
+  },
+  {
+    label: "Unsplash",
+    url: `https://unsplash.com/${handle}`,
+  },
+]
+
+export default links
