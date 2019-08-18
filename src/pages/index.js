33349c65df801c481eeef44bb0d8aba6053af890
Willian Justen <willianjustenqui@gmail.com>
2019-08-18 18:58:52 -0300

    Ordering posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import PostItem from "../components/PostItem"
 const IndexPage = () => {
   const { allMarkdownRemark } = useStaticQuery(graphql`
     query PostList {
-      allMarkdownRemark {
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
         edges {
           node {
             fields {
