e1605fd8e0c2ac977da062276a17f48f4054cd4e
Willian Justen <willianjustenqui@gmail.com>
2019-08-18 19:48:50 -0300

    Create blog list template

---
+++ src/templates/blog-list.js
@@ -0,0 +1,63 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import PostItem from "../components/PostItem"
+
+const BlogList = props => {
+  const postList = props.data.allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {postList.map(
+        ({
+          node: {
+            frontmatter: { background, category, date, description, title },
+            timeToRead,
+            fields: { slug },
+          },
+        }) => (
+          <PostItem
+            slug={slug}
+            background={background}
+            category={category}
+            date={date}
+            timeToRead={timeToRead}
+            title={title}
+            description={description}
+          />
+        )
+      )}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query PostList($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            background
+            category
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+            description
+            title
+          }
+          timeToRead
+        }
+      }
+    }
+  }
+`
+
+export default BlogList
