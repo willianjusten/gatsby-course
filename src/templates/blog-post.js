857ef4286340aa2f732b313b6002468eed039266
Willian Justen <willianjustenqui@gmail.com>
2019-08-18 17:52:30 -0300

    Create data to blog post

---
+++ src/templates/blog-post.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+const BlogPost = ({ data }) => {
+  const post = data.markdownRemark
+
+  return (
+    <>
+      <h1>Title: {post.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+    </>
+  )
+}
+
+export const query = graphql`
+  query Post($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+      }
+      html
+    }
+  }
+`
+
+export default BlogPost
