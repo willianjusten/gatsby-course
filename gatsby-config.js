50126e124245f143a683993b3c1371ed44230a64
Willian Justen <willianjustenqui@gmail.com>
2019-08-18 22:55:00 -0300

    Configure gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,14 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    // needs to be the first to work with gatsby-remark-images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `uploads`,
+        path: `${__dirname}/static/assets/img`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -25,7 +33,22 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [],
+        plugins: [
+          {
+            resolve: "gatsby-remark-relative-images",
+            options: {
+              name: "uploads",
+            },
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 960,
+              linkImagesToOriginal: false,
+            },
+          },
+          `gatsby-remark-lazy-load`,
+        ],
       },
     },
     `gatsby-transformer-sharp`,
--- src/components/Post/styled.js
+++ src/components/Post/styled.js
@@ -86,7 +86,6 @@ export const MainContent = styled.section`
   img {
     display: block;
     max-width: 100%;
-    margin: 1.875rem auto;
   }
 
   iframe {
