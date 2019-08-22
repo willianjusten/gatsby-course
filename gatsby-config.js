fc8d339c27849ae1455e70f62e2b397acfb8c616
Willian Justen <willianjustenqui@gmail.com>
2019-08-22 19:33:52 -0300

    Configure manifest, sitemap, seo, pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,17 +74,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `John Doe`,
+        short_name: `John Doe`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
