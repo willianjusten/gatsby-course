21d7fc352b6c253513a4f740abd6de63ccc3f55d
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-03-22 16:49:12 -0300

    Update algolia

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,34 +1,30 @@
 import React from "react"
 
+import algoliasearch from "algoliasearch/lite"
 import { InstantSearch, SearchBox, Hits, Stats } from "react-instantsearch-dom"
 
 import Hit from "./Hit"
 import * as S from "./styled"
 
-const algolia = {
-  appId: process.env.GATSBY_ALGOLIA_APP_ID,
-  searchOnlyApiKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-  indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
-}
+const Search = ({ algolia }) => {
+  const searchClient = algoliasearch(algolia.appId, algolia.searchOnlyApiKey)
 
-const Search = () => (
-  <S.SearchWrapper>
-    <InstantSearch
-      appId={algolia.appId}
-      apiKey={algolia.searchOnlyApiKey}
-      indexName={algolia.indexName}
-    >
-      <SearchBox autoFocus translations={{ placeholder: "Pesquisar..." }} />
-      <Stats
-        translations={{
-          stats(nbHits, timeSpentMs) {
-            return `${nbHits} resultados encontrados em ${timeSpentMs}ms`
-          },
-        }}
-      />
-      <Hits hitComponent={Hit} />
-    </InstantSearch>
-  </S.SearchWrapper>
-)
+  return (
+    <S.SearchWrapper>
+      <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
+        {/* eslint-disable-next-line jsx-a11y/no-autofocus */}
+        <SearchBox autoFocus translations={{ placeholder: "Pesquisar..." }} />
+        <Stats
+          translations={{
+            stats(nbHits, timeSpentMs) {
+              return `${nbHits} resultados encontrados em ${timeSpentMs}ms`
+            },
+          }}
+        />
+        <Hits hitComponent={Hit} />
+      </InstantSearch>
+    </S.SearchWrapper>
+  )
+}
 
 export default Search
--- src/pages/search.js
+++ src/pages/search.js
@@ -4,10 +4,16 @@ import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import Search from "../components/Search"
 
+const algolia = {
+  appId: process.env.GATSBY_ALGOLIA_APP_ID,
+  searchOnlyApiKey: process.env.GATSBY_ALGOLIA_SEARCH_ONLY_KEY,
+  indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+}
+
 const SearchPage = () => (
   <Layout>
     <SEO title="Search" />
-    <Search />
+    <Search algolia={algolia} />
   </Layout>
 )
 
